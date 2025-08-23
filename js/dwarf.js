35f0d585a2f02843230de410982aeb5e8f15cb61
JakeSeegers <Aikijake@gmail.com>
2025-08-23 09:44:27 -0400

    Update dwarf.js

--- js/dwarf.js
+++ js/dwarf.js
@@ -944,7 +944,7 @@ class Dworf {
                 case 'gym': urgency = Math.max(0, 25 - this.exercise); break;
                 case 'community_center': urgency = Math.max(0, 25 - this.social); break;
                 case 'spa': urgency = Math.max(0, 30 - this.cleanliness); break;
-                default: urgency = 2;
+                default: urgency = 5;
             }
             
             // Prefer closer buildings with high urgency
